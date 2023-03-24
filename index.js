0d73547bb29cefeb13d44eb173cdc06a59a8b296
MOHAMMAD ALMOHAMMAD <neotaiki123@gmail.com>
2023-03-24 19:37:53 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin (arr)
+{
+  const foundElement=arr.find(callBack)
+  return foundElement===undefined?foundElement:foundElement.year
+}
+
+function callBack(element)
+{
+  return element.result==="W"
+}
